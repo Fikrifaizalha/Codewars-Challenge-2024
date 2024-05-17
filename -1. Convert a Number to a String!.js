3e5cc619d5471e9bed51d7bfbfb67d779f873151
fikrifaizalha <fikrifaizalha@gmail.com>
2024-05-18 06:42:16 +0700

    coding challenge

---
+++ -1. Convert a Number to a String!.js
@@ -0,0 +1,12 @@
+// Fikri Faizal Hamzah Alfa'iz //
+
+// Codewars challenge 2024 //
+
+// https : https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+
+function numberToString(num) {
+  let str = "67";
+  return str.toString();
+}
+
+console.log(numberToString(67));
