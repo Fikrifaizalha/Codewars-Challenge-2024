c05576192d3a464d983d05708a91c5680553ac4b
fikrifaizalha <fikrifaizalha@gmail.com>
2024-05-18 08:28:36 +0700

    codewars

--- Basic Mathematical Operations (4).js
+++ Basic Mathematical Operations (4).js
@@ -4,6 +4,7 @@
 
 // https : https://www.codewars.com/kata/57356c55867b9b7a60000bd7
 
+// CARA KE 1 //
 function basicOp(operation, value1, value2) {
   if (operation === "+") {
     return value1 + value2;
@@ -15,6 +16,25 @@ function basicOp(operation, value1, value2) {
     return value1 / value2;
   }
   return 0;
+}
+
+console.log(basicOp("+", 4, 7));
+
+// CARA KE 2 //
+
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case "+":
+      return value1 + value2;
+    case "-":
+      return value1 - value2;
+    case "*":
+      return value1 * value2;
+    case "/":
+      return value1 / value2;
+    default:
+      return 0;
+  }
 }
 
 console.log(basicOp("+", 4, 7));
