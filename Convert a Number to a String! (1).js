68defefa3b91868b4d531c419268b8ff7659d5f1
fikrifaizalha <fikrifaizalha@gmail.com>
2024-05-18 07:19:29 +0700

    codewars

--- Convert a Number to a String! (1).js
+++ Convert a Number to a String! (1).js
@@ -4,9 +4,27 @@
 
 // https : https://www.codewars.com/kata/5265326f5fda8eb1160004c8
 
+// CARA KE 1
+
+// function numberToString(num) {
+//   const str = num.toString();
+//   return str.toString();
+// }
+
+// console.log(numberToString(67));
+
+// CARA KE 2
+
+// function numberToString(num) {
+//   return num + "";
+// }
+
+// console.log(numberToString(67));
+
+// CARA KE 3
+
 function numberToString(num) {
-  let str = "67";
-  return str.toString();
+  return num.toString();
 }
 
 console.log(numberToString(67));
