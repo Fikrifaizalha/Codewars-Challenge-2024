7c0b4c4c88f946c067c858caff9aa088eedbae5a
fikrifaizalha <fikrifaizalha@gmail.com>
2024-05-22 12:52:12 +0700

    codewars

--- 2-Even or Odd.js
+++ 2-Even or Odd.js
@@ -21,3 +21,14 @@ function checkOddOrEven(n) {
 
 console.log(checkOddOrEven(12));
 console.log(checkOddOrEven(121));
+
+// CARA KE 3:
+function checkOddOrEven(n) {
+  if (n & 1) {
+    return "Number is odd";
+  }
+  return "Number is even";
+}
+
+console.log(checkOddOrEven(12)); // Outputs: "Number is even"
+console.log(checkOddOrEven(121)); // Outputs: "Number is odd"
